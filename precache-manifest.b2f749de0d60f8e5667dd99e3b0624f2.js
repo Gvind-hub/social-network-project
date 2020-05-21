1d162086d27c5e327fda23ba22d3cc16cf64c4a0
Gvind-hub <63408287+Gvind-hub@users.noreply.github.com>
2020-05-21 22:51:40 +0300

    Updates

---
+++ precache-manifest.b2f749de0d60f8e5667dd99e3b0624f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62ff9b6a5a88fce7043a618f357e6a1",
+    "url": "/social-network-project/index.html"
+  },
+  {
+    "revision": "7416c6f2462ae7afaf9a",
+    "url": "/social-network-project/static/css/3.66704da9.chunk.css"
+  },
+  {
+    "revision": "06dc37044d4692526912",
+    "url": "/social-network-project/static/css/4.92c699fd.chunk.css"
+  },
+  {
+    "revision": "016f8573eec250dd9363",
+    "url": "/social-network-project/static/css/main.0f8789cb.chunk.css"
+  },
+  {
+    "revision": "686185f9d24aa02263a9",
+    "url": "/social-network-project/static/js/2.bb25d8ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network-project/static/js/2.bb25d8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7416c6f2462ae7afaf9a",
+    "url": "/social-network-project/static/js/3.02f8eb59.chunk.js"
+  },
+  {
+    "revision": "06dc37044d4692526912",
+    "url": "/social-network-project/static/js/4.d790b76a.chunk.js"
+  },
+  {
+    "revision": "016f8573eec250dd9363",
+    "url": "/social-network-project/static/js/main.f9d88b16.chunk.js"
+  },
+  {
+    "revision": "1898f29b0d3b3e549365",
+    "url": "/social-network-project/static/js/runtime-main.80abb6bd.js"
+  },
+  {
+    "revision": "888fa4eb0f6a7744ac7e05c2ca1e4729",
+    "url": "/social-network-project/static/media/icons-user.888fa4eb.png"
+  },
+  {
+    "revision": "3d1b739fb2546948f207d2be7ae1b105",
+    "url": "/social-network-project/static/media/preloader.3d1b739f.gif"
+  }
+]);
